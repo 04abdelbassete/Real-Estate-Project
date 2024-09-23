24bb4cceaacb8d6c21bfcafd89e8f44b3770c9f2
abdelbassete <abdo@email.com>
2024-09-24 00:06:26 +0100

    fix views.py

--- estate/users/forms.py
+++ estate/users/forms.py
@@ -1,16 +1,1 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
 
-
-User = get_user_model()
-
-class UserAccountForm(UserCreationForm):
-    
-    class Meta:
-        model = User
-        exclude = ('is_active', 'is_staff',)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(self,*args, **kwargs)
-        self.fields['is_realtor'].widget.attrs.update({'label':'Are you a realtor?'})
